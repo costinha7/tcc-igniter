f1866932d895ebf85fad5e2166006a8c534ed652
lucasmenchon <it.lucas@outlook.com>
2022-06-06 21:46:42 -0300

    add novas controllers

---
+++ app/Controllers/LivroController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class LivroController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
