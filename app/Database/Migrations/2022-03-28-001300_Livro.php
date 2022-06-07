4fd6397344e165ee54f5c531f713e80ce7c61f97
lucasmenchon <it.lucas@outlook.com>
2022-06-06 21:52:06 -0300

    add migration livro

--- app/Database/Migrations/2022-03-28-001300_Livro.php
+++ app/Database/Migrations/2022-03-28-001300_Livro.php
@@ -4,11 +4,43 @@ namespace App\Database\Migrations;
 
 use CodeIgniter\Database\Migration;
 
-class Livro extends Migration
+class Cliente extends Migration
 {
     public function up()
     {
-        //
+     $this->forge->addField([
+            'id_livro' => [
+            'type'       => 'VARCHAR',
+            'constraint' => 5,
+            'null'       => false,
+            'unsigned'   => true,
+            'auto_increment' => true,
+            ],
+            'titulo_livro'  => [
+            'genero' => 'VARCHAR',
+            'codigo' => 'VARCHAR',
+            'null' => false,
+            ],
+            'ex' => [
+            'type' => 'CHAR',
+            'constraint' => '11',
+            'null' => false,  
+            ],
+            'ex' =>[
+            'type' => 'DATE',
+             'null' => false,  
+            ],
+            'ex' =>[
+            'type' => 'CHAR',
+            'constraint' => '14',
+            'null' => false,  
+            ],
+            'ex' =>[
+            'type' => 'VARCHAR',
+             'constraint' => '50',
+             'null' => false,   
+            ],
+        ]);
     }
 
     public function down()
